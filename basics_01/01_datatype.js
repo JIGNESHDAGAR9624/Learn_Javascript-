0dbbf5dbf002e8fac529bd2f45c14bda8b49ad31
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-23 06:34:13 +0000

    datatype

---
+++ basics_01/01_datatype.js
@@ -0,0 +1,7 @@
+
+
+let name = "jignesh" // string value
+let Number = 9857656 // number value
+let isLoggedIn = true // boolean value
+let undefined ; // value not define that is called undefined
+
---
+++ basics_01/01_variable.js
@@ -0,0 +1,16 @@
+ const accountId = 964565 // Immutable
+ let accountEmial = "jignesh@google.com" // for let intialization
+ var accountPassword = "12345"
+ accountCity = "Surat"   
+ let AccountType; // for Let Intialization 
+
+ // accountId = 1 //Not Changable
+ accountEmial = "dagar@gmail.com"
+ accountPassword = 43215
+accountCity = "Ahmedabad"
+AccountType = "saving"
+
+
+
+
+ console.table([accountId,accountEmial,accountPassword,accountCity , AccountType]);
