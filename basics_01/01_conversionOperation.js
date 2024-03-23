edd1ee25873379abe21ef440d63611b0bc3eadb5
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-23 08:31:18 +0000

    Conversion Of Data  Type

---
+++ basics_01/01_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = true 
+let isLoggesdIn =1;
+let isLoggesdn =0;
+
+//console.log(typeof(score));
+//console.log(typeof score);
+
+let valueInNumber = Number(score) // Convert to number
+//console.log(valueInNumber);
+
+let booleanInLoggedIntrue = Boolean(isLoggesdIn);
+//console.log(booleanInLoggedIntrue); 
+
+let booleanInLoggedInfalse= Boolean(isLoggesdn);
+//console.log(booleanInLoggedInfalse);
+
+let someNumber = 33;;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
