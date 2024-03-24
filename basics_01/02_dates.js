823d83621ae6f8186cec88383f7fa6912133cc81
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-24 11:43:59 +0000

    date and Time

---
+++ basics_01/02_dates.js
@@ -0,0 +1,12 @@
+
+
+let mydates = new Date();
+
+// console.log(mydates.toString());
+// console.log(mydates.getDay());
+// console.log(mydates.getDate());
+
+let myCreatedate = new Date(2024,2,24 , 5,11); //last two value(5 , 11) for hour and minuit  
+
+console.log(myCreatedate.toDateString());
+console.log(myCreatedate.toLocaleString()); 
