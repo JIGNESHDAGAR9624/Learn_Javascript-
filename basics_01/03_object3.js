ccc798d1c015ebfb23f154e5dd7b5dcad2bc6584
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-24 22:20:04 +0000

    object destctctor , api

---
+++ basics_01/03_object3.js
@@ -0,0 +1,16 @@
+const course ={
+    coursename:"javascript",
+    price:"999",
+    corseInstructor:"hitesh"
+}
+
+const {corseInstructor: instructor} = course;  // destructor object
+// console.log(corseInstructor);
+console.log(instructor);
+
+
+// what is Api(Application programming interface) :- apna kam dusro se karvana that is called  Api
+// => APIs are used to integrate new applications with existing software systems. 
+
+
+// Json (javascript Object Notation)
