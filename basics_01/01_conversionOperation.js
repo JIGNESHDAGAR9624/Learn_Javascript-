0e5d349ab49b6345263677365b14021af37e5f30
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-24 10:47:43 +0000

    strings keywords

--- basics_01/01_conversionOperation.js
+++ basics_01/01_conversionOperation.js
@@ -19,3 +19,34 @@ let someNumber = 33;;
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+
+// Stack(primitive)   Heap(non_primitive)
+// primitive datatype = number , string , boolean , bigInt
+// non Primitive datatype = object , array
+
+// Stack(primitive) 
+let myname = "jignesh";
+
+let mynewName = myname;
+
+mynewName = "Jigs";
+
+console.log(myname);
+console.log(mynewName);
+
+
+// Heap(non-Primitive) ,object
+let user ={
+    username: "jignesh",
+    userEmail: "jignesh@google.com",
+}
+
+let newuser = user;
+
+newuser.userEmail = "jignesh@amazone.com";
+console.log(user);
+console.log(newuser);
+
+
---
+++ basics_01/02_strings.js
@@ -0,0 +1,8 @@
+const gamename = new String("jignesh");
+
+console.log(gamename.charAt(2)); // define charchter position
+console.log(gamename.length); // For length
+console.log(gamename.toLowerCase());// to get lower case value
+console.log(gamename.toUpperCase()); // to get Uppercase value
+console.log(gamename.indexOf('n')); // position of chararcter
+console.log(gamename.substring(0,4));// 0 to 4 character print
