25fa9ea4e24cf9eed93bde23cb33be3706c7e78f
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-25 03:30:15 +0000

    function , array , object

---
+++ basics_01/03_function_restOperator.js
@@ -0,0 +1,30 @@
+function calculatePrice(val1 , val2 , ...num1){ //  val1 add 100 val2 add 200 and remaining value add rest Operator 
+    return num1;
+}
+
+//console.log(calculatePrice(100,200,300,400));
+
+
+// const user = {
+//     username: "jigensh",
+//     price: 222
+// }
+
+// function handleObject(anbyObject){
+//     console.log(`any :name  is ${anbyObject.username} and price is ${anbyObject.price}`);
+// }
+// handleObject(user)
+
+// function handlesimpleObjectt(){
+//     console.log(`any :name  is ${user.username} and price is ${user.price}`);
+// }
+// handlesimpleObjectt(user)
+
+
+// get array value
+const myarr =[200 , 300 ,400 , 500]
+function returnarray(getArray){
+   return getArray[1]
+}
+// console.log(returnarray(myarr));
+console.log(returnarray([100,200,3000,4000])); //get array with function
