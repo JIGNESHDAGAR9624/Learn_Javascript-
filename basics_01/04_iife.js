6966031d76f63c20be5e8bb708152187222dd183
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-26 05:35:15 +0000

    iife functin with arrow

---
+++ basics_01/04_iife.js
@@ -0,0 +1,18 @@
+// Immediately Invoked Function Expressions (IIFE)
+//IIFEs prevent pollution of the global JS scope.
+// function Immedatly Excute using IIFE
+
+// (
+// function chai(){
+//     console.log("DB Connected");
+// })();  Semi Colon For End
+
+
+// IIFE Arrow Function
+// ( () => {
+//     console.log("Db Connect Arrow Function");
+// })()
+
+// ( (name) => {
+//         console.log(`Db Connect Arrow Function  ${name}`)
+//      })("jigauu")
