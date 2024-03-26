fa2278a7f7bc0c8537674a4a7e6a7d784366adb1
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-26 15:05:10 +0000

    Switch Statement

---
+++ basics_01/04_switch.js
@@ -0,0 +1,25 @@
+
+
+
+// Switch case:- switch statement is used to perform different actions based on different conditions
+
+const month = "April";
+
+switch(month){
+    case 1:
+        console.log("january")
+        break;
+        
+    case 2:
+        console.log("Fabruary")
+        break;
+    
+    case 3:
+        console.log("March")
+        break;
+    
+    case "April":
+        console.log("April")
+        break;
+        
+}
--- basics_01/test.js
+++ basics_01/test.js
@@ -1,1 +1,12 @@
-console.log("Hii Jignesh Dagar");
+let val1 = 10;
+let val2 = 20;
+
+function addNum(num1 , num2){
+    let total = num1 + num2;
+    return total;
+  //console.log("The Total Value is" ,total); if i romve return and add console then print otherwise not
+}
+
+let result1 = addNum(val1 , val2);
+
+console.log("result is",result1);
