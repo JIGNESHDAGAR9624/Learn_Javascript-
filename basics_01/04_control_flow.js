b28275ed6aa1be220b4d05bbf627968e551b0f55
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-26 13:31:04 +0000

    if codition and operator

---
+++ basics_01/04_control_flow.js
@@ -0,0 +1,25 @@
+//< less then value
+// > greter then value
+// >= greterthan or is Equal
+// <= less or Equal
+// == eual value only
+// != not Equal (3 != 2) true 
+// === check type(string , number , boolean) and value(2 === "jignesh") false value value (2 === 2) true
+// !== not eual type and value
+
+
+// if(2 !== "2"){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+// const score = 200;
+
+// if(score > 100){
+//     const power ="fly"
+//    console.log(`score is ${power}`);
+//    console.log(`score is ${score}`);
+// }
+// console.log(`score is ${score}`);
+
