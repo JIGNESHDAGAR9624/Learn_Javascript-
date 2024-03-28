c55037f6c69f3e6226d96a548eca6508bec26612
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-28 23:46:06 +0000

    for each loop and map

---
+++ basics_01/07_highOrder_Array_loops.js
@@ -0,0 +1,49 @@
+// for of loop :- The forEach loop is a method in JavaScript that allows you to iterate over an array and perform an operation
+                  // on each element of the array.
+                  //For each loop not use in object
+
+//[" " , " " , " "]
+//[{" " , " " , " "}]
+
+//const arr = [1 , 2 , 3 , 4 , 5 ]
+
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+
+
+// const strarr = "Hello World";
+
+// for(const str of strarr){
+//     console.log(`Each char is ${str}`)
+// }
+
+
+// Maps 
+// not Add Dublicate Value in Map
+const map = new Map()
+map.set(`IN` , "India" )
+map.set(`Fr` , "France")
+map.set(`USA` , "United States Of America")
+map.set(`IN` , "India" )
+
+
+// console.log(map);
+
+
+// for(const [key , value ] of map){
+//     console.log(key ," :- " , value);
+// }
+
+
+// const myObject = {
+//     game1 : `Mario`,
+//     game2 : `SpiderMan`
+// }
+
+// for (const [key ,value]  of myObject) {
+//     console.log(key, `:-` , value);
+// }
+
+
