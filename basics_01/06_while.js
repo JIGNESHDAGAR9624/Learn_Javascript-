a6f2c4ccb8e0cde21f129200cc6fafbbf50fb71a
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-28 00:38:37 +0000

    while , do while

---
+++ basics_01/06_while.js
@@ -0,0 +1,28 @@
+ 
+ //While Loop:- The while statement creates a loop (araund a code block) that is executed while a condition is true 
+ //,The loop runs while the condition is true . Otherwise it stops.
+
+// let index =0
+
+// while(index <=10){
+//     console.log(`The Value Of Index is ${index}`);
+//     index = index + 2;
+// }
+
+// let myarray = ["flash" , "batman" , "superman" , "saktiman"];
+
+// let arr = 0;
+// while(arr < myarray.length){
+//     console.log(`The Array Value is ${myarray[arr]}`);
+//     arr++;
+// }
+
+
+
+//do While loop :-  to repeat a specific block of code an unknown number of times, until a condition is met.
+
+let score =0;
+do{
+ console.log(`The value of Score is ${score}`);
+ score++;
+}while(score <= 10);
