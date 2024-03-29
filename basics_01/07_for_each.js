099037bb27f9519b87b27e5c722eb47157bafb87
Jignesh Dagar <89568583+JIGNESHDAGAR9624@users.noreply.github.com>
2024-03-29 02:24:58 +0000

    for each , object

--- basics_01/07_for_each.js
+++ basics_01/07_for_each.js
@@ -12,8 +12,46 @@ const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
 // coding.forEach( (val) => {
 //    console.log(val);
 // })
+  
+// when i use (item , index ,and arr ) add in console (js ,0,all arry value)
+// function printme(item , index , arr){
+//     console.log(item , index , arr);
+// }
+// coding.forEach(printme)
 
-function printme(item){
-    console.log(item);
-}
-coding.forEach(printme)
+// arraow call back function
+//  coding.forEach((item , index , arr) => {
+//  console.log(item,index,arr);
+//  })
+
+
+
+
+//use array object in for each
+\
+ const mycoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "ruby",
+        languageFileName: "rB"
+    },
+    
+    {
+        languageName: "cpp",
+        languageFileName: "c++"
+    },
+    {
+        languageName: "css",
+        languageFileName: "Case cading style sheets"
+    },
+ ]
+
+
+ mycoding.forEach( (item) =>{
+    console.log(item.languageFileName)
+    console.log(item.languageName)
+ })
+
